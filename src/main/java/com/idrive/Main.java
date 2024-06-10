bfe28951d85766cef5bca3317ce3e724b7e51b72
Lucas Barros <lbs12@aluno.ifal.edu.br>
2024-06-10 19:23:33 -0300

    add: models e services

--- src/main/java/com/idrive/Main.java
+++ src/main/java/com/idrive/Main.java
@@ -2,6 +2,6 @@ package com.idrive;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bem vindo a Idrive!");
+        System.out.println("Bem vindo a Idrive! ");
     }
 }
